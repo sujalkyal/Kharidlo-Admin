8649009f8d5d1c9c309161e27b4279cb29bda8fb
sujalkyal <sujalkyal2704@gmail.com>
2025-03-07 00:40:12 +0530

    dashboard redirect

--- app/page.jsx
+++ app/page.jsx
@@ -1,9 +1,5 @@
-import React from 'react';
+import { redirect } from "next/navigation";
 
 export default function Home() {
-  return (
-    <div>
-      This is Admin Page
-    </div>
-  );
+  redirect("/dashboard");
 }
